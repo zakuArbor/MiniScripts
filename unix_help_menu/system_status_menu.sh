2bb8535b07ab6979e0cfb7ddade7b716dbc3a23c
zakuArbor <nabimoon1234@gmail.com>
2017-07-20 16:48:00 -0400

    Updated option 4 Description

--- unix_help_menu/system_status_menu.sh
+++ unix_help_menu/system_status_menu.sh
@@ -1,7 +1,7 @@
 echo -e "\tSYSTEM STATUS COMMANDS"
 echo "1 -- Display the current date and time"
 echo "2 -- Current disk usage"
-echo "3 -- List current local and environmental"
+echo "3 -- List current environment variables"
 echo "4 -- Display process status information"
 echo "5 -- Quit -- Return to Main Menu"
 echo -ne "\tEnter your choice:"
