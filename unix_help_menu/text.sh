3ec9601c48b307ff063d18bcc00e6f2c08fe1397
zakuArbor <nabimoon1234@gmail.com>
2017-07-21 00:44:53 -0400

    Added basic flow of text commands with grep option finished

---
+++ unix_help_menu/text.sh
@@ -0,0 +1,67 @@
+usage="usage: $0 [option pattern file]"
+file_error=": No such file"
+if test $# -gt 3
+then
+	echo $usage >&2 
+	exit 1
+fi
+if test $# -eq 0
+then
+    sh text_processing_menu.sh
+    read cmd
+else
+	cmd=$1
+fi
+if test $# -gt 2
+then
+	file=$3
+fi
+if test "$cmd" -gt 0 && test "$cmd" -lt 4 && test $# -lt 2 
+then
+	echo -ne "\tEnter file name: "
+	read file
+fi
+
+if (test "$cmd" -ge 1) && (test "$cmd" -le 3) && ! test -f $file
+then
+	echo $file$file_error >&2
+	exit 1
+fi
+
+case "$cmd" in
+	"1") #Search a file for a pattern
+		grep $2 $file
+	;;
+	"2") #Count lines, words, and characters in specified files
+		wc $file
+	;;
+	"3") #Differences between two files
+		if test $# -ne 3
+		then
+			echo -ne "\tEnter file 2 name: "
+			read 'file2'
+			if ! test -f $file2
+			then
+				echo $file2$file_error >&2
+				exit 1
+			fi
+		else
+			$file2=$file #swap file and file 2
+			$file=$2
+			if ! test -f $file
+			then
+				echo $file$file_error >&2
+				exit 1
+			fi
+		fi
+		diff $file $file2
+	;;
+	"4") #Return to main menu
+		sh main.sh
+	;;
+	*)
+		echo $usage >&2
+		exit 1
+	;;
+esac
+
