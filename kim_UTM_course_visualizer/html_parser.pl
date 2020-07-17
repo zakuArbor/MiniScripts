c14bbd64c82fdbef2b3573f829b9e99106682ea3
Ju Hong Kim <nabimoon1234@gmail.com>
2020-07-16 22:07:00 -0400

    [Parser] Able to parse a semester

--- kim_UTM_course_visualizer/html_parser.pl
+++ kim_UTM_course_visualizer/html_parser.pl
@@ -16,14 +16,17 @@ sub strip_beginning {
 sub parse_academic_history {
 	my $fh = shift or die "File Hanle is undefined";
 	my $terminator_re = qr/<\/history-academic>/;
-	my $course_html_re = qr/<div class="courses\s/;
+	my $course_html_re = qr/.*<div class="courses\s.*">/;
 	my $delimiter_re = qr/\s+/;
 	my $course_code_re = qr/\w{3}\d{3}[H|Y][1|5]/;
-	my $course_name_re = qr/(\w+\s?)+/;
+	my $course_name_re = qr/([\w+\s?]+)/;
 	my $weight_re = qr/0\.50|1\.00/;
-	my $letter_grade_re = qr/[ABCD][-+]?/;
-	my $course_code;
-	my $course_name;
+	my $mark_re = qr/\d+|\s{2}/; #Note: this script will not capture your Mark in the course, just your grade (i.e. letter grade) 
+	my $letter_grade_re = qr/[ABCD][-+]?|CR|NCR/;
+	my $course_re = qr/(${course_code_re})\s+(${course_name_re})\s+(${weight_re})\s+${mark_re}\s+(${letter_grade_re})\s+(${letter_grade_re})/;
+	my $sem_end_re = qr/<\/div>/;
+	my $course_code
+;	my $course_name;
 	my $course_name2;
 	my $course_grade;
 	my $course_avg;
@@ -36,23 +39,44 @@ sub parse_academic_history {
 		if ($line =~ $terminator_re) {
 			return 1;
 		}
-		if ($state == 0) {
-			if (($course_code, $course_name, undef, $weight, $course_grade, $course_avg) = $line =~ /${course_html_re}.*(${course_code_re})\s+(${course_name_re})\s+(${weight_re})\s+\d+\s+(${letter_grade_re})\s+(${letter_grade_re})\s+/) {
-				print $line;
+
+		
+		if ($state == 0 && $line =~ /${course_html_re}/) {
+			$line =~ s/${course_html_re}//g;
+			$state++;
+		}
+		
+		if ($state > 0) {
+			if ($line =~ /$sem_end_re/) {
+				$state = 0;
+			}
+
+			if (($course_code, $course_name, undef, $weight, $course_grade, $course_avg) = $line =~ /$course_re/) {
 				print $course_code . "\n";
 				print "${course_name}\n";
 				print "${weight}\n";
 				print "${course_grade}\n";
 				print "${course_avg}\n";
-				$state++;
+				$state = 2;
 			}
-		}
-		elsif ($state == 1) {
-			#if course name is too long that it goes to another line
-			if (($course_name2) = $line =~ /^\s+${course_name_re}\s+$/) {
-				print "${course_name2}\n";
+			elsif ($state == 2) {
+				
+				#if course name is too long that it goes to another line
+				if (($course_name2) = $line =~ /^\s+${course_name_re}\s+$/) {
+					print "course2: ${course_name2}\n";
+				}
+				else { #another course
+					print("test");
+					if (($course_code, $course_name, undef, $weight, $course_grade, $course_avg) = $line =~ /$course_re/) {
+						print $course_code . "\n";
+						print "${course_name}\n";
+						print "${weight}\n";
+						print "${course_grade}\n";
+						print "${course_avg}\n";
+					}
+				}
+				$state = 1;
 			}
-			$state++;
 		}
 	}
 }
