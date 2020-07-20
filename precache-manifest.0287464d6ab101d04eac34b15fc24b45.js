b3d688527a9afc95f819dd607e0cae8e3dd62232
Ju Hong Kim <nabimoon1234@gmail.com>
2020-07-20 19:23:19 -0400

    Updates

---
+++ precache-manifest.0287464d6ab101d04eac34b15fc24b45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ab13e946c7615737041f8de1076928",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "889bdd1441f9074f7d32",
+    "url": "/portfolio/static/css/main.5a8418a0.chunk.css"
+  },
+  {
+    "revision": "5fec308d1a6039d1fde6",
+    "url": "/portfolio/static/js/2.6ee090e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.6ee090e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889bdd1441f9074f7d32",
+    "url": "/portfolio/static/js/main.451b596a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
